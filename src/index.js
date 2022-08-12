f6e50ee2b5d1a4299b346674c952a639656eb75b
Dito Kharismawan <dito.kharismawan94@gmail.com>
2022-08-12 13:33:04 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+const name = "Angela";
+const number = 7;
 
-
-ReactDom.render(<h1>My Favourite Foods</h1>,document.getElementById("root"));
+ReactDom.render(
+  <div>
+    <h1>Hello {name}!</h1>
+    <p>Your lucky number is {number} </p>
+      
+   </div>,
+   document.getElementById("root")
+   );
